2359f18449f406e0f3538e600d5c72d302b00551
thanapat <thanapat.war@ku.th>
2021-08-07 20:01:46 +0700

    add Relation to exchange

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 3000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'dabcc7f6d38a7059edd97fa22b51a367'),
